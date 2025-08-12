f147ddf528e6289dfd0e7a3a61adb277d4ac5a87
Khaled Abou Hechmeh <6320936@cegepmv.ca>
2025-08-12 17:38:47 -0400

    Added brain background

--- illithid_powers/src/App.jsx
+++ illithid_powers/src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Power from "./power/Power";
+import illithidIdentification from "./assets/photos/illithid_identification.png";
+import brain from "./assets/photos/brain.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-radial from-red-950 to-black h-[925px] w-full flex overflow-hidden">
+      <img
+        className="absolute right-0 left-0 top-0 bottom-0 m-auto scale-200 z-1"
+        src={brain}
+      />
+      <div className="z-2"></div>
+    </div>
   );
 }
 
---
+++ illithid_powers/src/power/Power.jsx
@@ -0,0 +1,9 @@
+export default function Power(props) {
+  return (
+    <>
+      <div className="bg-stone-900 rounded-full border-pink-500 border-2 size-20 z-2">
+        <img src={props.link} />
+      </div>
+    </>
+  );
+}
